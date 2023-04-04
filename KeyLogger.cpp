dbd3d91bbb660ddbfbb6e618ae7b460ef00719ac
Darlan <darlaneduardoguimaraes@gmail.com>
2023-04-04 13:59:23 -0300

    Init

---
+++ KeyLogger.cpp
@@ -0,0 +1,6 @@
+//
+// Created by darla on 04/04/2023.
+//
+
+#include "KeyLogger.h"
+
---
+++ KeyLogger.h
@@ -0,0 +1,14 @@
+//
+// Created by darla on 04/04/2023.
+//
+
+#ifndef KEYSTROKELOGGER_KEYLOGGER_H
+#define KEYSTROKELOGGER_KEYLOGGER_H
+
+
+class KeyLogger {
+
+};
+
+
+#endif //KEYSTROKELOGGER_KEYLOGGER_H
